dbafc89d7e79695d9bba3f6fdb9f11a09a9af653
J0113boy <keskitalo_6@hotmail.com>
2024-07-08 15:37:49 +0200

    starting with loops

--- Javascript/exercise/loops.js
+++ Javascript/exercise/loops.js
@@ -1,23 +1,109 @@
 // FOR LOOPS
 
 // Skapa en loop som itererar 1000 varv. För varje iteration ska du console.log() vilket varv du är på.
-
 // Skapa en loop som räknar ner från 100 till 0. För varje varv ska du console.log() vilket varv du är på.
-
-
 // Loopa ut ovanstående array med en for-loop. För varje varv ska du console.log() innehållet ( frukten ).
-
 // Loopa ut ovanstående array med console.log(). Sätt vilket index varje frukt har framför. Ex. 0. apple, 1. orange osv.
-
 // Skapa en enkel tabell för multiplikation av ett nummer, till exempel 7:console.log() sjuans gångertabell Ex.
+// Sök i en array: Skapa en funktion som tar en array och ett sökord som argument och returnerar alla element som innehåller sökordet.
+const testArray = ['Joel', 'Amanda', 'Tim', 'Patrik']
+
+for (let i = 0; i < testArray.length; i++) {
+    console.log(testArray[i])
+}
+
+for(let i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+
+const fruits = ['Apple', 'Banana', 'Pear', 'Orange']
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`${i}.${fruits[i]}`)
+}
+
+
+for(let i = 2; i <= 20; i++) {
+    console.log(i)
+}
+
+for(let i = 5; i <= 50; i++) {
+    console.log(i)
+}
+
+for(let i = 0; i <= 7; i++) {
+    console.log(i * 7)
+}
+
 
 // loopa ut föjande array och skriv ut det på sidan
 
-// Sök i en array: Skapa en funktion som tar en array och ett sökord som argument och returnerar alla element som innehåller sökordet.
+const movieArray = ['Dune', 'The Fellowship of the Ring', 'American Gangster', 'Toy Story']
+
+for (let i = 0; i < movieArray.length; i++) {
+    console.log(movieArray[i])
+}
+
+
+
+
+class Movie {
+    constructor(title, director, imdbRating, runtime) {
+        this.title = title
+        this.director = director
+        this.imdbRating = imdbRating
+        this.runtime = runtime
+    }
+}
+
+class MovieLibrary {
+    constructor() {
+        this.movies = []
+
+    }
+
+    addMovie(movie) {
+        return this.movies.push(movie)
+    }
+
+    searchTitle(titleSearch) {
+        if (typeof titleSearch !== 'string' || titleSearch.length === 0) {
+            return []
+        } else {
+            titleSearch = titleSearch.charAt(0).toUpperCase() + titleSearch.slice(1).toLowerCase()
+            this.movies.filter((movie) => movie.title.includes(titleSearch))
+            console.log(`${titleSearch} found!`)
+        }
+    }
+
+    removeMovie(movieToRemove) {
+        movieToRemove = movieToRemove.charAt(0).toUpperCase() + movieToRemove.slice(1).toLowerCase()
+        this.movies = this.movies.filter((movie) => movie.title !== movieToRemove)
+        console.log(`Movie successfully deleted: ${movieToRemove}`)
+    }
+
+}
+
+const library = new MovieLibrary()
+library.addMovie(new Movie("The Shawshank Redemption", "Frank Darabont", 9.3, "142 min"))
+console.log(library)
+library.searchTitle('the shawshank redemption')
+library.removeMovie()
+
+new Movie("The Godfather", "Francis Ford Coppola", 9.2, "175 min")
+new Movie("The Dark Knight", "Christopher Nolan", 9.0, "152 min")
+new Movie("12 Angry Men", "Sidney Lumet", 9.0, "96 min")
+new Movie("Schindler's List", "Steven Spielberg", 8.9, "195 min")
+
+
+
+
+
 
 // Hitta högsta numret i en array med en For loop
 
-//  Loopa ut ovanstående array och console.log() varje pryl.
+// Loopa ut ovanstående array och console.log() varje pryl.
 
 // Loopa ut ovanstående Object och console.log() varje key.
 
@@ -33,3 +119,5 @@
 
 // Skapa en while-loop som kör 1000 varv. console.log() ut varje varv.
 
+
+// FOR OF ... LOOPS
--- Javascript/exercise/objects.js
+++ Javascript/exercise/objects.js
@@ -5,6 +5,10 @@
 // Lägg till en metod i Book-klassen som tar en procentuell rabatt som argument och sänker priset på boken med denna rabatt.
 // Skapa en metod i Book-klassen som returnerar true om boken är skriven av en viss författare (som passeras som argument), annars false.
 // Lägg till en metod i Book-klassen som jämför antalet sidor med en annan bok (som också är ett Book-objekt) och returnerar vilken bok som har fler sidor.
+// Skapa en metod i Book-klassen som returnerar true om boken är publicerad av ett visst förlag (som passeras som argument), annars false.
+// Lägg till en metod i Book-klassen som tar ett antal exemplar som argument och returnerar totalpriset för detta antal böcker.
+// Skapa en metod i Book-klassen som jämför priset med en annan bok (som också är ett Book-objekt) och returnerar vilken bok som är dyrare.
+
 
 class Book {
     constructor(title, author, publisher, numPages, price) {
@@ -15,12 +19,12 @@ class Book {
         this.price = price
     }
 
-    bookDescription = () => {
+    bookDescription() {
         const description = `${this.title}, ${this.author}, ${this.publisher}, ${this.numPages}, ${this.price}`
         return description
     }
 
-    pagesController = (pages) => {
+    pagesController(pages) {
         parseInt(pages)
         if(this.numPages > pages) {
             return console.log('Your book is longer than ' + pages)
@@ -29,7 +33,7 @@ class Book {
         }
     }
 
-    updatePrice = (newPrice) => {
+    updatePrice(newPrice) {
         if(isNaN(newPrice)) {
             return console.log('Give a correct number')
         }
@@ -37,19 +41,19 @@ class Book {
         return console.log('The pricing on your book has been updated to: ' + newPrice)
     }
 
-    pricePerPage = () => {
+    pricePerPage() {
         const calculation = this.numPages / this.price
         return calculation
     }
 
-    percentDiscount = (percent) => {
+    percentDiscount(percent) {
         const decimalPercent = percent / 100
         const discount = this.price * decimalPercent
         const newPrice = this.price - discount
         return newPrice
     }
 
-    searchAuthor = (author) => {
+    searchAuthor(author) {
         if(this.author.includes(author)) {
             return console.log('Congratulations, you found the correct author')
         } else {
@@ -57,18 +61,45 @@ class Book {
         }
     }
 
-    comparePages = (theOtherBook) => {
+    comparePages(theOtherBook) {
         if(this.numPages > theOtherBook.numPages) {
             return `${this.title} has more pages than your ${theOtherBook.numPages}!`
         } else {
             return `God damn you! ${theOtherBook.numPages} is more pages than ${this.title} has!`
         }
     }
+
+    checkPublisher(searchPublisher) { // joels f00king book man
+        if(this.publisher.includes(searchPublisher)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    totalPrice(bookOne, bookTwo) {
+        const totalPrice = bookOne.price + bookTwo.price
+        return console.log(totalPrice)
+    }
+
+    comparePrice(secondBook) {
+
+        let comparisonResult = this.price - secondBook.price
+
+        if(this.price > secondBook.price) {
+            return console.log(`${this.title} is more expensive than ${secondBook.title} by ${comparisonResult} SEK!`)
+        } else if(secondBook.price > this.price) {
+            return console.log(`${secondBook.title} is more expensive than ${this.title} by ${comparisonResult} SEK!`)
+        }
+    }
 }
 
 
 const joelsBook = new Book('The Book of Luls', 'J.Keskitalo', 'Noob AB', 600, 300)
-const dennisBook = new Book('The Book of Madao', 'D.Jansson', 'Kabuki District', 500, 250)
+const dennisBook = new Book('The Book of Madao', 'D.Jansson', 'Kabuki District', 500, 450)
+
+console.log(joelsBook.comparePrice(dennisBook))
+
 
 console.log(joelsBook.bookDescription())
 console.log(joelsBook.pagesController('500'))
@@ -79,8 +110,7 @@ console.log(joelsBook.percentDiscount(25))
 console.log(joelsBook.searchAuthor('D.Jansson'))
 console.log(joelsBook.comparePages(dennisBook))
 console.log(dennisBook.comparePages(joelsBook))
+console.log(joelsBook.checkPublisher('Noob AB'))
+console.log(joelsBook.totalPrice(joelsBook, dennisBook))
 
 
-// Skapa en metod i Book-klassen som returnerar true om boken är publicerad av ett visst förlag (som passeras som argument), annars false.
-// Lägg till en metod i Book-klassen som tar ett antal exemplar som argument och returnerar totalpriset för detta antal böcker.
-// Skapa en metod i Book-klassen som jämför priset med en annan bok (som också är ett Book-objekt) och returnerar vilken bok som är dyrare.
