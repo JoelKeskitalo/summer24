ac1843596e348b1d9b57cf4344caf24c98a616d5
J0113boy <keskitalo_6@hotmail.com>
2024-07-10 19:51:23 +0200

    done for today, 0710

---
+++ Javascript/maximilianJavascriptRecap/scripts/script.js
@@ -0,0 +1,11 @@
+import { apiKey } from './utils.js'
+
+// import apiKey from './utils.js
+// import { apiKey, abc as content } from './utils.js
+
+// import * as util from './utils.js
+
+
+// console.log(util.)
+// console.log(content)
+console.log(apiKey)
---
+++ Javascript/maximilianJavascriptRecap/scripts/utils.js
@@ -0,0 +1,3 @@
+export let apiKey = 'abcdefghijklmnopqrstuvwxyz'
+
+// export default 'abcdefghijklmnopqrstuvwxyz'
