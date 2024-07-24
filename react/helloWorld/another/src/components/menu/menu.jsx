8eff527a7e733558c6075a26df260d48db5e3186
J0113boy <keskitalo_6@hotmail.com>
2024-07-24 20:06:52 +0200

    done for today

--- react/helloWorld/another/src/components/menu/menu.jsx
+++ react/helloWorld/another/src/components/menu/menu.jsx
@@ -1,7 +1,22 @@
+import "./Menu.scss"
+
 function Menu() {
   return (
     <div className="main-box">
-      <h1>Menu</h1>
+      <h1>Baratheons Beverages</h1>
+      <div id="menu-box">
+        <nav>
+          <ul>
+            <li>Example</li>
+            <li>Example</li>
+            <li>Example</li>
+            <li>Example</li>
+            <li>Example</li>
+            <li>Example</li>
+            <li>Example</li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
