0078880ce656199083dc780dca8041c71d02cc7b
J0113boy <keskitalo_6@hotmail.com>
2024-08-13 13:24:03 +0200

    early template for home page

--- warhammer/warhammer40k/src/components/Home/Home.jsx
+++ warhammer/warhammer40k/src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
 import "./Home.scss"
+import warhammerLogo from "../../assets/warhammer40klogo.png"
+import goldenAquila from "../../assets/goldenaquila.png"
 
 function Home() {
-  return <div className="home-content"></div>
+  return (
+    <div className="home-content">
+      <img src={warhammerLogo} id="warhammer-logo" />
+      <img src={goldenAquila} id="aquila-logo" />
+    </div>
+  )
 }
 
 export default Home
